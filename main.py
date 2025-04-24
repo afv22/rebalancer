27dd306fe92cb175999eda368bd8f4586fcafc57
Andrew Vagliano <andrew.vagliano1@gmail.com>
2025-04-24 15:13:33 -0400

    Add schwab API connection

--- main.py
+++ main.py
@@ -1,25 +1,56 @@
+import os
+import dotenv
+from schwab import auth
+
 from src.get_rebalance_amounts import get_rebalance_amounts
+from src.send_email import send_email
+
 
 def main():
     # Fetch holdings
+    api_key = os.getenv("SCHWAB_API_KEY")
+    app_secret = os.getenv("SCHWAB_SECRET")
+
+    if not api_key or not app_secret:
+        raise ValueError("Schwab API details not found")
+
+    c = auth.easy_client(
+        api_key=api_key,
+        app_secret=app_secret,
+        callback_url="https://127.0.0.1:8182",
+        token_path="/tmp/token.json",
+    )
+
+    accounts = c.get_accounts(fields=[c.Account.Fields.POSITIONS])
     holdings = []
+    for account in accounts.json():
+        if "positions" not in account["securitiesAccount"]:
+            continue
+        for position in account["securitiesAccount"]["positions"]:
+            holding = {
+                "asset": position["instrument"]["symbol"],
+                "value": position["marketValue"],
+            }
+            holdings.append(holding)
 
     # Process allocations
-    target_allocations = {}
+    target_allocations = {"VOO": 0.55, "VXUS": 0.30, "GLDM": 0.05, "VGIT": 0.10}
     rebalance_amounts = get_rebalance_amounts(holdings, target_allocations)
 
     # Check if any asset is outside the allowed window
     for asset in rebalance_amounts:
-        if asset['absolute_difference'] >= 0.05:
+        if asset["absolute_difference"] >= 0.05:
             break
-        if asset['relative_difference'] >= 0.25:
+        if asset["relative_difference"] >= 0.25:
             break
     else:
         return
 
     # Alert me or automatically rebalance
-    pass
+    print("Time to rebalance!")
+    # send_email()
 
-    
+
 if __name__ == "__main__":
+    dotenv.load_dotenv()
     main()
--- src/send_email.py
+++ src/send_email.py
@@ -2,16 +2,17 @@ import dotenv
 import smtplib
 from email.mime.text import MIMEText
 
+
 def send_email(subject, body):
     sender = "andrew.vagliano1@gmail.com"
     recipient = "andrew.vagliano1@gmail.com"
-    password = dotenv.get_key('GMAIL_APP_PASSWORD')
-    
+    password = dotenv.get_key("GMAIL_APP_PASSWORD")
+
     msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = recipient
-    
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = recipient
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(sender, password)
         server.sendmail(sender, recipient, msg.as_string())
