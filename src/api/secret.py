bde5b1135cba657eeb20e797df17924e0db921a9
Andrew Vagliano <andrew.vagliano1@gmail.com>
2025-05-14 18:49:11 -0400

    Directly access project iD

--- src/api/secret.py
+++ src/api/secret.py
@@ -5,18 +5,15 @@ from google.cloud import secretmanager
 
 class GoogleSecretWrapper:
     _client = None
-    _pid = None
 
     @classmethod
     def get_secret(cls, secret_name: str):
         if cls._client is None:
             cls._client = secretmanager.SecretManagerServiceClient()
 
-        if cls._pid is None:
-            cls._pid = os.environ.get("GOOGLE_PROJECT_ID")
-
         try:
-            secret_name = f"projects/{cls._pid}/secrets/{secret_name}/versions/latest"
+            pid = os.environ.get("PROJECT_ID")
+            secret_name = f"projects/{pid}/secrets/{secret_name}/versions/latest"
             response = cls._client.access_secret_version(request={"name": secret_name})
             return response.payload.data.decode("UTF-8")
 
